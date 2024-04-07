7fb7079388c8202f59f0c526c869e691d4cf4c91
Nguyen Kim Huy <kimhuy011199@gmail.com>
2024-04-07 20:29:36 +0700

    Create blog homepage

---
+++ src/config/constant.js
@@ -0,0 +1,18 @@
+export const POST_PER_PAGE = 3;
+
+export const RELATED_POSTS_PER_PAGE = 2;
+
+export const MIN_MATCH_CHAR_LENGTH = 3;
+
+export const DEFAULT_PAGE_TITLE = 'Blog huynk.tech';
+
+export const DEFAULT_PAGE_DESC =
+  'Explore the world of web development, JavaScript, and more with insightful articles and tutorials.';
+
+export const DEFAULT_PAGE_AUTHOR = 'Huy Nguyen Kim';
+
+export const DEFAULT_SOCIAL_IMAGE_URL = 'huynk-og.webp';
+
+export const APP_NAME = 'huynk.tech';
+
+export const APP_DOMAIN = 'https://huynk.tech';
