77999f4e2b3459f7bec88b927dc14b1abda2e4c6
Nguyen Kim Huy <kimhuy011199@gmail.com>
2024-04-13 18:09:48 +0700

    Upgrade rss content

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -2,8 +2,9 @@ import rss, { pagesGlobToRssItems } from '@astrojs/rss';
 
 export async function GET(context) {
   return rss({
-    title: 'Astro Learner | Blog',
-    description: 'My journey learning Astro',
+    title: 'Blog huynk.tech',
+    description:
+      'Explore the world of web development, JavaScript, and more with insightful articles and tutorials.',
     site: context.site,
     items: await pagesGlobToRssItems(import.meta.glob('./**/*.mdx')),
     customData: `<language>en-us</language>`,
